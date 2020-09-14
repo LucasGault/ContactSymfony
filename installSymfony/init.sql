1a149cb14ab1e08cb0e11fb89850622e76e06435
Gault Lucas <lucasgault@MC-G-LP-C120-14.local>
2020-09-14 17:30:56 +0200

    Docker et symfony propre

--- installSymfony/init.sql
+++
@@ -1,4 +0,0 @@
-create database if not exists contactony;
-use contactony;
-
-CREATE TABLE if not exists contact ( id INT NOT NULL AUTO_INCREMENT , firstname VARCHAR(255) NOT NULL , lastname VARCHAR(255) NOT NULL , phonenumber INT(10) NOT NULL , PRIMARY KEY (id)) ENGINE = InnoDB;
