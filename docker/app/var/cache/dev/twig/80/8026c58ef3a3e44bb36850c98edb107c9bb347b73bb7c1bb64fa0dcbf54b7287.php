fcfced81f7e73b3cc07c2c63fc4b42e040e97e7d
Emma Zn <emmanuelle.zenou.rogala@gmail.com>
2020-09-17 11:36:44 +0200

    front dev

--- docker/app/var/cache/dev/twig/80/8026c58ef3a3e44bb36850c98edb107c9bb347b73bb7c1bb64fa0dcbf54b7287.php
+++ docker/app/var/cache/dev/twig/80/8026c58ef3a3e44bb36850c98edb107c9bb347b73bb7c1bb64fa0dcbf54b7287.php
@@ -57,42 +57,39 @@ class __TwigTemplate_2e353dbaded92111695397d0814c54c235dfe1965f31b090ba912115bc7
 
         // line 3
         echo "
-<div class=\"head\">
-    <h1>Mes Contacts</h1>
-<div>
 
-<div class=\"page\">
+<div class=\"page\" style=\"margin:5%\">
     <div class=\"container\">
         ";
-        // line 10
+        // line 7
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["contacts"]) || array_key_exists("contacts", $context) ? $context["contacts"] : (function () { throw new RuntimeError('Variable "contacts" does not exist.', 10, $this->source); })()));
+        $context['_seq'] = twig_ensure_traversable((isset($context["contacts"]) || array_key_exists("contacts", $context) ? $context["contacts"] : (function () { throw new RuntimeError('Variable "contacts" does not exist.', 7, $this->source); })()));
         foreach ($context['_seq'] as $context["_key"] => $context["contact"]) {
             echo " 
         <div class=\"row justify-content-center\">
             <div class=\"col col-lg-6\">
-                <div class=\"card mb-4\" id=\"contactCard\" >
-                    <div class=\"card-header\">
+                <div class=\"shadow-lg card mb-4\" id=\"contactCard\" >
+                    <div class=\"shadow card-header\">
                         <h5 class=\"card-title\">";
-            // line 15
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["contact"], "firstname", [], "any", false, false, false, 15), "html", null, true);
+            // line 12
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["contact"], "firstname", [], "any", false, false, false, 12), "html", null, true);
             echo " ";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["contact"], "lastname", [], "any", false, false, false, 15), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["contact"], "lastname", [], "any", false, false, false, 12), "html", null, true);
             echo "</h5>
                     </div> 
                     <div class=\"card-body\">
                         <span> 0";
-            // line 18
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["contact"], "phonenumber", [], "any", false, false, false, 18), "html", null, true);
+            // line 15
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["contact"], "phonenumber", [], "any", false, false, false, 15), "html", null, true);
             echo " </span>
                     </div>
                 </div>
             </div>
-            <div class=\"col col-lg-1\">
+            <div class=\"contactButtons\">
                 <button type=\"button\" class=\"close\" aria-label=\"Close\">
                     <span aria-hidden=\"true\" >&times;</span>
                 </button>
-                <svg style=\"width: 50%; margin:10%\" aria-hidden=\"true\" data-prefix=\"far\" data-icon=\"edit\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\" class=\"svg-inline--fa fa-edit fa-w-18 fa-3x\"><path fill=\"teal\" d=\"M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z\" class=\"\"></path></svg>
+                    <svg style=\"width: 50%; left:20%\" aria-hidden=\"true\" data-prefix=\"far\" data-icon=\"edit\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\" class=\"svg-inline--fa fa-edit fa-w-18 fa-3x\"><path fill=\"#d8b011\" d=\"M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z\" class=\"\"></path></svg>    
             </div>         
         </div>
         ";
@@ -100,7 +97,7 @@ class __TwigTemplate_2e353dbaded92111695397d0814c54c235dfe1965f31b090ba912115bc7
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['contact'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 30
+        // line 27
         echo "    </div>
 </div>
 
@@ -122,7 +119,7 @@ class __TwigTemplate_2e353dbaded92111695397d0814c54c235dfe1965f31b090ba912115bc7
 
     public function getDebugInfo()
     {
-        return array (  104 => 30,  86 => 18,  78 => 15,  68 => 10,  59 => 3,  52 => 2,  35 => 1,);
+        return array (  101 => 27,  83 => 15,  75 => 12,  65 => 7,  59 => 3,  52 => 2,  35 => 1,);
     }
 
     public function getSourceContext()
@@ -130,17 +127,14 @@ class __TwigTemplate_2e353dbaded92111695397d0814c54c235dfe1965f31b090ba912115bc7
         return new Source("{% extends 'base.html.twig' %}
 {% block body %}
 
-<div class=\"head\">
-    <h1>Mes Contacts</h1>
-<div>
 
-<div class=\"page\">
+<div class=\"page\" style=\"margin:5%\">
     <div class=\"container\">
         {% for contact in contacts %} 
         <div class=\"row justify-content-center\">
             <div class=\"col col-lg-6\">
-                <div class=\"card mb-4\" id=\"contactCard\" >
-                    <div class=\"card-header\">
+                <div class=\"shadow-lg card mb-4\" id=\"contactCard\" >
+                    <div class=\"shadow card-header\">
                         <h5 class=\"card-title\">{{contact.firstname}} {{contact.lastname}}</h5>
                     </div> 
                     <div class=\"card-body\">
@@ -148,11 +142,11 @@ class __TwigTemplate_2e353dbaded92111695397d0814c54c235dfe1965f31b090ba912115bc7
                     </div>
                 </div>
             </div>
-            <div class=\"col col-lg-1\">
+            <div class=\"contactButtons\">
                 <button type=\"button\" class=\"close\" aria-label=\"Close\">
                     <span aria-hidden=\"true\" >&times;</span>
                 </button>
-                <svg style=\"width: 50%; margin:10%\" aria-hidden=\"true\" data-prefix=\"far\" data-icon=\"edit\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\" class=\"svg-inline--fa fa-edit fa-w-18 fa-3x\"><path fill=\"teal\" d=\"M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z\" class=\"\"></path></svg>
+                    <svg style=\"width: 50%; left:20%\" aria-hidden=\"true\" data-prefix=\"far\" data-icon=\"edit\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\" class=\"svg-inline--fa fa-edit fa-w-18 fa-3x\"><path fill=\"#d8b011\" d=\"M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9 216.2 301.8l-7.3 65.3 65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1 30.9-30.9c4-4.2 4-10.8-.1-14.9z\" class=\"\"></path></svg>    
             </div>         
         </div>
         {% endfor %}
