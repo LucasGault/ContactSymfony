7e12f4df0d82898165c0bb0e157a9d8fc8f0452f
Emma Zn <emmanuelle.zenou.rogala@gmail.com>
2020-09-23 19:24:56 +0200

    front dev , search function and delete function OK

--- docker/app/src/Controller/ContactController.php
+++ docker/app/src/Controller/ContactController.php
@@ -1,19 +1,37 @@
 <?php
 
 namespace App\Controller;
+
 use App\Entity\Contact;
 use App\Repository\ContactRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Component\HTTPFoundation\Response;
 
 class ContactController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(ContactRepository $contactRepository)
+    public function index(ContactRepository $contactRepository) : Response 
     {
         $contacts = $contactRepository->findBy(array(), array('firstname'=>'ASC'));
         return $this->render('contact/index.html.twig',compact('contacts'));
+    }
+
+    /**
+     * @Route("/delete/{id}", name="app_contact_delete", methods={"DELETE"})
+     */
+    public function delete($id,EntityManagerInterface $em): Response
+    {
+        $contact = $this->getDoctrine()
+        ->getRepository(Contact::class)
+        ->find($id);
+
+        $em->remove($contact);
+        $em->flush();
+        return $this->redirectToRoute('home');
     }
 }
--- docker/app/var/cache/dev/url_matching_routes.php
+++ docker/app/var/cache/dev/url_matching_routes.php
@@ -13,11 +13,13 @@ return [
     [ // $regexpList
         0 => '{^(?'
                 .'|/_error/(\\d+)(?:\\.([^/]++))?(*:35)'
+                .'|/delete/([^/]++)(*:58)'
             .')/?$}sDu',
     ],
     [ // $dynamicRoutes
-        35 => [
-            [['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null],
+        35 => [[['_route' => '_preview_error', '_controller' => 'error_controller::preview', '_format' => 'html'], ['code', '_format'], null, null, false, true, null]],
+        58 => [
+            [['_route' => 'app_contact_delete', '_controller' => 'App\\Controller\\ContactController::delete'], ['id'], ['DELETE' => 0], null, false, true, null],
             [null, null, null, null, false, false, 0],
         ],
     ],
